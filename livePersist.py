125c4999efbf7b701a0e1d523b405065ea024e10
zachhanson94 <zachhanson94@gmail.com>
2019-05-15 23:00:03 -0700

    Fixes

--- livePersist.py
+++ livePersist.py
@@ -1,25 +1,22 @@
+#! /usr/bin/env python3
 import subprocess
 
 
 subprocess.run(["fdisk", "-l"])
-print("")
-print("") 
-print("") 
-   
-device = input("What is the name of the storage device? (Ex. sda, sdb, sdc): ") 
- 
- 
-# Create Storage partition 
- 
-subprocess.run(["fdisk", "/dev/" + device]) 
- 
+print("\n\n\n")
+
+device = input("What is the name of the storage device? (Ex. sda, sdb, sdc): ")
+
+
+# Create Storage partition
+subprocess.run(["fdisk", "/dev/" + device])
 # Encrypt persistent storage
 
 subprocess.run(["fdisk", "-l"])
 
 part = input("What is the name of the storage partition? (Ex. sdb1, sdb2): ")
 
-subprocess.run(["cryptsetup","-v", "-y", "luksFormat", "/dev/" + part])
+subprocess.run(["cryptsetup", "-v", "-y", "luksFormat", "/dev/" + part])
 
 subprocess.run(["cryptsetup", "luksOpen", "/dev/" + part, "my_usb"])
 
@@ -31,6 +28,4 @@ subprocess.run(["mkdir", "-p", "/mnt/my_usb"])
 
 subprocess.run(["mount", "/dev/mapper/my_usb", "/mnt/my_usb"])
 
-subprocess.run(["echo", '"/ union" > /mnt/my_usb/persistence.conf'])
--- (insert) VISUAL --                       11        5,10          Top
-
+subprocess.run(["echo", "\"/ union \" > /mnt/my_usb/persistence.conf'])
