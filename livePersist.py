d6491db3df06b98c901719b69c1b957e234868b1
9ur9le <9ur9lekpg@gmail.com>
2019-05-16 01:16:03 -0400

    First commit

---
+++ livePersist.py
@@ -0,0 +1,36 @@
+import subprocess
+
+
+subprocess.run(["fdisk", "-l"])
+print("")
+print("") 
+print("") 
+   
+device = input("What is the name of the storage device? (Ex. sda, sdb, sdc): ") 
+ 
+ 
+# Create Storage partition 
+ 
+subprocess.run(["fdisk", "/dev/" + device]) 
+ 
+# Encrypt persistent storage
+
+subprocess.run(["fdisk", "-l"])
+
+part = input("What is the name of the storage partition? (Ex. sdb1, sdb2): ")
+
+subprocess.run(["cryptsetup","-v", "-y", "luksFormat", "/dev/" + part])
+
+subprocess.run(["cryptsetup", "luksOpen", "/dev/" + part, "my_usb"])
+
+subprocess.run(["mkfs.ext4", "-L persistence", "/dev/mapper/my_usb"])
+
+subprocess.run(["e2label", "/dev/mapper/my_usb", "persistence"])
+
+subprocess.run(["mkdir", "-p", "/mnt/my_usb"])
+
+subprocess.run(["mount", "/dev/mapper/my_usb", "/mnt/my_usb"])
+
+subprocess.run(["echo", '"/ union" > /mnt/my_usb/persistence.conf'])
+-- (insert) VISUAL --                       11        5,10          Top
+
