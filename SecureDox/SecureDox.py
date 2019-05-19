578d4fc33a92d6f58cabb13b580f13e133e4847e
9ur9le <9ur9lekpg@gmail.com>
2019-05-19 05:27:44 -0400

    script update

--- SecureDox/SecureDox.py
+++ SecureDox/SecureDox.py
@@ -4,40 +4,47 @@ import subprocess
 
 # Choose Encryption or Decryption
 
-answer = input("Encrypt or Decrypt?\n ")
+answer = input("Encrypt[1] or Decrypt[2]?\n ")
 
 # Encryption
 
-if (answer == "Encrypt"):
+if (answer == "1"):
 
-  data = input("What is the path of the data file you would like to encrypt?\n ")
+    data = input("DATA FILE NAME:\n ")
 
-  new_file = input("What would you like to call the new encrypted file?\n ")
+    new_file = input("WHAT WOULD YOU LIKE TO NAME THE NEWLY ENCRYPTED FILE?\n ")
 
-  pubkey = input("What is the path of the public key?\n ")
-
-
-  subprocess.run(["openssl", "rand","-out", "randompass.txt", "-base64", "120"])
-
-  subprocess.run(["openssl", "enc", "-aes-256-cbc", "-salt", "-in", data, "-out",   new_file, "-pass", "file:randompass.txt"])
+    pubkey = input("RECIPIENTS PUBLIC KEY:\n ")
 
 
-  subprocess.run(["openssl", "rsautl","-encrypt", "-in", "randompass.txt", "-out"  , "randompass.enc", "-pubin", "-inkey",  pubkey])
+    subprocess.run(["openssl", "rand","-out", "randompass.txt", "-base64", "120"])
+
+    subprocess.run(["openssl", "enc", "-aes-256-cbc", "-salt", "-in", data, "-out",   new_file, "-pass", "file:randompass.txt"])
+
+    subprocess.run(["openssl", "rsautl","-encrypt", "-in", "randompass.txt", "-out"  , "randompass.enc", "-pubin", "-inkey",  pubkey])
+
+    
+    subprocess.run(["rm", "randompass.txt"])
+    subprocess.run(["mv", data, "msg"])
 
 # Decryption
 
-elif (answer == "Decrypt"):
+elif (answer == "2"):
 
-  privkey = input("What is the path to your private key?\n ")
+   privkey = input("ENTER YOUR PRIVATE KEY:\n ")
 
-  file_name = input("What is the path of the encrypted data file?\n ")
+   file_name = input("ENTER THE NAME OF THE ENCRYPTED FILE:\n ")
 
-  data = input("What would you like to name the new data file?\n ")
+   data = input("NAME THE NEW FILE:\n ")
 
-  subprocess.run(["openssl", "rsautl", "-decrypt", "-inkey", privkey, "-in", "randompass.enc","-out", "decodedpass.txt"])
+   subprocess.run(["openssl", "rsautl", "-decrypt", "-inkey", privkey, "-in", "randompass.enc","-out", "decodedpass.txt"])
 
 
-  subprocess.run(["openssl","enc", "-d", "-aes-256-cbc", "-in", file_name,"-out",  data, "-pass", "file:decodedpass.txt"])
+   subprocess.run(["openssl","enc", "-d", "-aes-256-cbc", "-in", file_name,"-out",  data, "-pass", "file:decodedpass.txt"])
+  
+   subprocess.run(["rm", "decodedpass.txt",
+"keys"])
+   subprocess.run(["rm", "randompass.enc"])
 
 else:
   print("Try Again. Input is case sensitive")
